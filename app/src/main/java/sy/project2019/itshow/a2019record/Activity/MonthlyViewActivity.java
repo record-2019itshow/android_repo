0d2c52a6565cf9c0c342e7c1a4a49b3baafc3f36
송승연 <wnzk413@gmail.com>
2019-06-10 01:17:16 +0900

    캘린더 커스텀

--- app/src/main/java/sy/project2019/itshow/a2019record/Activity/MonthlyViewActivity.java
+++ app/src/main/java/sy/project2019/itshow/a2019record/Activity/MonthlyViewActivity.java
@@ -7,6 +7,11 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashSet;
+
 import sy.project2019.itshow.a2019record.R;
 
 public class MonthlyViewActivity extends AppCompatActivity {
@@ -34,4 +39,41 @@ public class MonthlyViewActivity extends AppCompatActivity {
         });
         //액션바 설정 코드 끝
     }
+
+
+
+    // 캘린더의 날짜를 업데이트하는 함수
+//    public void updateCalendar(HashSet<Date> events)
+//    {
+//        ArrayList<Date> cells = new ArrayList<>();
+//        Calendar calendar = (Calendar)currentDate.clone();
+//
+//        // determine the cell for current month's beginning
+//        calendar.set(Calendar.DAY_OF_MONTH, 1);
+//        int monthBeginningCell = calendar.get(Calendar.DAY_OF_WEEK) - 1;
+//
+//        // move calendar backwards to the beginning of the week
+//        calendar.add(Calendar.DAY_OF_MONTH, -monthBeginningCell);
+//
+//        // fill cells
+//        while (cells.size() < DAYS_COUNT)
+//        {
+//            cells.add(calendar.getTime());
+//            calendar.add(Calendar.DAY_OF_MONTH, 1);
+//        }
+//
+//        // update grid
+//        grid.setAdapter(new CalendarAdapter(getContext(), cells, events));
+//
+//        // update title
+//        SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
+//        txtDate.setText(sdf.format(currentDate.getTime()));
+//
+//        // set header color according to current season
+//        int month = currentDate.get(Calendar.MONTH);
+//        int season = monthSeason[month];
+//        int color = rainbow[season];
+//
+//        header.setBackgroundColor(getResources().getColor(color));
+//    }
 }
